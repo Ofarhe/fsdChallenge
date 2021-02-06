44ae27ffd9c450711a71c4859abf337ef2f07e32
Javier Pimentel <78445534+Ofarhe@users.noreply.github.com>
2021-02-05 20:45:41 -0600

    Correct query and security issues
    Corrected security issues on query and input text.
    Fixed item list diaplaying extra items.
    Pending displaying issues on cards.

--- src/query.js
+++ src/query.js
@@ -4,8 +4,10 @@ const api_url_data = "https://restcountries.eu/rest/v2/all?fields=name;region;po
 var itemCount = 0;
 
 async function showCardItem(countryName, regionName, population, languagesName, currenciesName) {
-    //Grab data to from JSON file
-    /*var countryName = "Country Name";
+    //Receives data from request
+
+    /* Testing code
+    var countryName = "Country Name";
     var regionName = "Region Name";
     var currenciesName = "Currencies Name";
     var languagesName = "Languages Name";
@@ -15,11 +17,11 @@ async function showCardItem(countryName, regionName, population, languagesName, 
     // Open cardHolder div to add elements
     var cardHolder = document.getElementById("cardHolder");
 
-    // Crate dummy elements to fill and add parameters
-    //Assign the corresponding ID's and class to the elements
+    //  Crate dummy elements to fill and add parameters to them
+    //      Assign the corresponding ID's and class to the elements
     var cardItem = document.createElement("table");
-    cardItem.setAttribute('id', "hiddenCardItem");
-    cardItem.setAttribute('class', "cardItem");
+        cardItem.setAttribute('id', "hiddenCardItem");
+        cardItem.setAttribute('class', "cardItem");
     var cardItemBody = document.createElement("tbody");
     var cardItemNameRow = document.createElement("tr");
     var cardItemRegionRow = document.createElement("tr");
@@ -27,27 +29,27 @@ async function showCardItem(countryName, regionName, population, languagesName, 
     var cardItemLangRow = document.createElement("tr");
     var cardItemCurrRow = document.createElement("tr");
     var cardNameHead = document.createElement("th");
-    cardNameHead.setAttribute('class', "theader");
+        cardNameHead.setAttribute('class', "theader");
     var cardNameItem = document.createElement("th");
-    cardNameItem.setAttribute('class', "tcontent");
+        cardNameItem.setAttribute('class', "tcontent");
     var cardRegionHead = document.createElement("th");
-    cardRegionHead.setAttribute('class', "theader");
+        cardRegionHead.setAttribute('class', "theader");
     var cardRegionItem = document.createElement("th");
-    cardRegionItem.setAttribute('class', "tcontent");
+        cardRegionItem.setAttribute('class', "tcontent");
     var cardPopHead = document.createElement("th");
-    cardPopHead.setAttribute('class', "theader");
+        cardPopHead.setAttribute('class', "theader");
     var cardPopItem = document.createElement("th");
-    cardPopItem.setAttribute('class', "tcontent");
+        cardPopItem.setAttribute('class', "tcontent");
     var cardLangHead = document.createElement("th");
-    cardLangHead.setAttribute('class', "theader");
+        cardLangHead.setAttribute('class', "theader");
     var cardLangItem = document.createElement("th");
-    cardLangItem.setAttribute('class', "tcontent");
+        cardLangItem.setAttribute('class', "tcontent");
     var cardCurrHead = document.createElement("th");
-    cardCurrHead.setAttribute('class', "theader");
+        cardCurrHead.setAttribute('class', "theader");
     var cardCurrItem = document.createElement("th");
-    cardCurrItem.setAttribute('class', "tcontent");
+        cardCurrItem.setAttribute('class', "tcontent");
 
-    //Assembles the cardItem element table with corresponding format
+    //Assembles the 'cardItem' table with corresponding format
     cardItem.appendChild(cardItemBody);
     cardItemBody.appendChild(cardItemNameRow);
     cardItemNameRow.appendChild(cardNameHead);
@@ -65,7 +67,7 @@ async function showCardItem(countryName, regionName, population, languagesName, 
     cardItemCurrRow.appendChild(cardCurrHead);
     cardItemCurrRow.appendChild(cardCurrItem);
 
-    // Adds the values to the cardItem
+    // Adds the values and fills the cardItem
     cardNameHead.appendChild(document.createTextNode("Name"));
     cardNameItem.appendChild(document.createTextNode(countryName));
     cardRegionHead.appendChild(document.createTextNode("Region"));
@@ -78,7 +80,7 @@ async function showCardItem(countryName, regionName, population, languagesName, 
     cardCurrItem.appendChild(document.createTextNode(currenciesName));
 
 
-    //Adds the new table to the cardContainer Div
+    //Adds the new table to the cardContainer element
     cardHolder.appendChild(cardItem);
 }
 
@@ -106,6 +108,31 @@ async function getCountryData() {
             country_curr[j] = data[i].currencies[j].name;
         }
     }
+}
+
+async function getData(api_url) {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    var countryData = [];
+    var countryDataLocal = [];
+    countryLang = "";
+    countryCurr = "";
+
+    for (j = 0; j < (data.length) / 5; j++) {
+        for (i = 0; i < data[j].languages.length; i++) {
+            countryLang = countryLang.concat(data[j].languages[i].name, " ");
+        }
+        for (i = 0; i < data[j].currencies.length; i++) {
+            countryCurr = countryCurr.concat(data[j].currencies[i].name, " ");
+        }
+        countryDataLocal[0] = data[j].name;
+        countryDataLocal[1] = data[j].region;
+        countryDataLocal[2] = data[j].population;
+        countryDataLocal[3] = countryLang;
+        countryDataLocal[4] = countryCurr;
+        countryData[j] = countryDataLocal;
+    }
+    return countryData;
 }
 
 async function getAllData() {                       // Gets all data from all countries.
@@ -141,75 +168,82 @@ async function getAllData() {                       // Gets all data from all co
 
 }
 
-async function getData(api_url) {
-    const response = await fetch(api_url);
-    const data = await response.json();
-    var countryData = [];
-    var countryDataLocal = [];
-    countryLang = "";
-    countryCurr = "";
-
-    for (j = 0; j < (data.length)/5; j++){
-        for (i = 0; i < data[j].languages.length; i++) {
-            countryLang = countryLang.concat(data[j].languages[i].name, " ");
-        }
-        for (i = 0; i < data[j].currencies.length; i++) {
-            countryCurr = countryCurr.concat(data[j].currencies[i].name, " ");
-        }
-        countryDataLocal[0] = data[j].name;
-        countryDataLocal[1] = data[j].region;
-        countryDataLocal[2] = data[j].population;
-        countryDataLocal[3] = countryLang;
-        countryDataLocal[4] = countryCurr;
-    countryData[j] = countryDataLocal;
+async function showDialogAlert(){
+    var securityAlertItem = document.getElementById("securityAlert");
+    if(typeof securityAlertItem.showModal === "function") {
+        securityAlertItem.showModal();
+    } else {
+        alert("Your browser does not support dialog element!");
     }
-    return countryData;
+
+}
+async function closeAlert(){
+    document.getElementById("securityAlert").close();
 }
 
 async function getCountryByName() {                              /* Creates a query to retreive name */
-    var name_url = "https://restcountries.eu/rest/v2/name/";
-    var countryQuery = document.getElementById("simpleSearchBar").value;
-    var new_name_url = name_url.concat(countryQuery);      //Retrieves the text as it gets clicked
-    var countryDataLocal = [];
-    var countryData = await getData(new_name_url);
-    try {
-        removeCardItems();
-    } catch (e) {
-        logMyErrors(e);
-    }
-    //Get elements in the response and print them
-        for (i = 0; i < countryData.length; i++){  
-            countryDataLocal = countryData[i];      
-            showCardItem(countryDataLocal[0], countryDataLocal[1], countryDataLocal[2], countryDataLocal[3], countryDataLocal[4]);
+
+    var inputList = document.getElementById("simpleSearchBar");                 // Call the searchbar
+    var check = inputList.value;                                                // Call the value of the searchbar
+    if (check.length <= 2) { 
+        // Security thing
+        showDialogAlert();
+        console.log("SecurityAlert");
+    } else if (check.length > 2){
+        try {
+            removeCardItems();
+        } catch (e) {
+            logMyErrors(e);
         }
-
-}
-
-async function searchCountryByName() {                              /* Creates a query to retreive name */
-    var check = "";
-    check = document.getElementById("simpleSearchBar").value;
-    if (check.length >= 2) {
-
-        https://restcountries.eu/rest/v2/name/can?fields=name
 
         var name_url = "https://restcountries.eu/rest/v2/name/";
         var countryQuery = document.getElementById("simpleSearchBar").value;
-        var new_name_url = name_url.concat(countryQuery, "?fields=name");      //Retrieves the text as it gets clicked
-        try {
-            const response = await fetch(new_name_url);
-            const data = await response.json();
+        var new_name_url = name_url.concat(countryQuery);      //Retrieves the text as it gets clicked
+        var countryDataLocal = [];
+        var countryData = await getData(new_name_url);
+        //Get elements in the response and print them
+        for (i = 0; i < countryData.length; i++) {
+            countryDataLocal = countryData[i];
+            showCardItem(countryDataLocal[0], countryDataLocal[1], countryDataLocal[2], countryDataLocal[3], countryDataLocal[4]);
+        }
+    }
+}
 
-            for (var let in data) {
+async function searchCountryByName() {                                          /* Creates a query to retreive name */
+    var inputList = document.getElementById("simpleSearchBar");                 // Call the searchbar
+    var check = inputList.value;                                                // Call the value of the searchbar
+    if (check.length > 2) {                                                     // Check the length of the word,
+        //check = document.getElementById("simpleSearchBar").value;
+        // https://restcountries.eu/rest/v2/name/can?fields=name
+        
+        var name_url = "https://restcountries.eu/rest/v2/name/";                //Assembles the query
+        var countryQuery = document.getElementById("simpleSearchBar").value;    // ...
+        var new_name_url = name_url.concat(countryQuery, "?fields=name");       // Assembles the value entered
+        
+        try {
+            const response = await fetch(new_name_url);                         // Fetches the query made by the user
+            const data = await response.json();                                 //..
+
+            // Clears existing list and shows new datalist of countries 
+            document.getElementById("countriesName").remove();                  //Clears the existing list of countries
+
+            var dataListItem = document.createElement("datalist");              //Creates the list element countries
+            dataListItem.setAttribute('id', "countriesName");                   //..
+            document.getElementById("simpleSearchBar").append(dataListItem);    //..
+
+            for (var let in data) {                                             // Fills the list element with matching
+                console.log(countryQuery)                                       // Countries
                 var optionElement = document.createElement("option");
+                optionElement.setAttribute("id", "optionItem");
                 optionElement.value = data[let].name;
-                document.getElementById("countriesName").appendChild(optionElement);
+                countriesName.appendChild(optionElement);
             }
         }
         catch (error) {
             console.error(error)
         }
     }
-    else {
+    else {                                                                      //If query length is less than 2,
         var optionElement = document.createElement("option");
         document.getElementById("countriesName").innerHTML = "";
 
